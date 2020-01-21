a7390bcbdcdeb69045750c6153837409a65fa2c0
GFatigate <gustavo.fatigate@gmail.com>
2020-01-21 15:01:41 -0300

    Create precache-manifest.66a33f61500f217244f9667f0c441449.js

---
+++ build/precache-manifest.66a33f61500f217244f9667f0c441449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac0e1efdf5005e242227da533f9b4bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef79e8ce9ff120f6cedf",
+    "url": "/static/css/main.c36aa7c8.chunk.css"
+  },
+  {
+    "revision": "781c16e3f0e9493e2c25",
+    "url": "/static/js/2.065c5f30.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.065c5f30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef79e8ce9ff120f6cedf",
+    "url": "/static/js/main.513c9d83.chunk.js"
+  },
+  {
+    "revision": "d644ff198816bb9f0669",
+    "url": "/static/js/runtime-main.c0627188.js"
+  }
+]);
